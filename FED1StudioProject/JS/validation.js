2f280fffa454e3757761762d627347b9a8499fe8
Ryan Palmer <rjpalmer9888@gmail.com>
2021-06-18 11:30:53 -0500

    Updated Contact

--- FED1StudioProject/JS/validation.js
+++ FED1StudioProject/JS/validation.js
@@ -1,39 +1,39 @@
 ﻿function validateForm(event) {
     //Gather the HTML form-control elements into a collection so that we don't have to
     //create a variable for each one.
-    let controls = document.getElementsByClassName("form-control");
+    let controls = document.getElementsByClassPhone("form-control");
     //console.log(controls);
 
-    let validationMessages = document.getElementsByClassName("invalid");
+    let validationMessages = document.getElementsByClassPhone("invalid");
 
     //Regular expression for email:
     let rxpEmail = new RegExp(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
 
-    //Regex for name"
-    let rxpName = new RegExp(/^[a-zA-Z]+$/);
+    //Regex for phone"
+    let rxpPhone = new RegExp(/^[a-zA-Z]+$/);
 
     //Check the length of the text in all of the controls to ensure that there is at least
     //one character in each field. If there isn't, stop the submission and fire a
     //validation error to the page
 
-    if (controls['name'].value.length == 0 || controls['email'].value.length == 0 ||
-        controls['subject'].value.length == 0 || controls['message'].value.length == 0 ||
-        !rpxEmail.test(controls['email'].value) || !rxpName.test(controls['name'].value)) {
+    if (controls['phone'].value.length == 0 || controls['email'].value.length == 0 ||
+        controls['address'].value.length == 0 ||
+        !rxpEmail.test(controls['email'].value) || !rxpPhone.test(controls['phone'].value)) {
         event.preventDefault();
 
         //Check individual controls
-        //Name Validation
-        if (controls['name'].value.length == 0) {
-            validationMessages['rfvName'].textContent = "Name is required";
+        //Phone Validation
+        if (controls['phone'].value.length == 0) {
+            validationMessages['rfvPhone'].textContent = "Phone is required";
         }
         else {
-            validationMessages['rfvName'].textContent = "";
+            validationMessages['rfvPhone'].textContent = "";
         }
-        if (!rxpName.test(controls['name'].value) && controls['name'].value.length > 0) {
-            validationMessages['rfvName'].textContent = "Name must only contain alphabetic characters";
+        if (!rxpPhone.test(controls['phone'].value) && controls['phone'].value.length > 0) {
+            validationMessages['rfvPhone'].textContent = "Phone must not contain alphabetic characters";
         }
-        if (rxpName.test(controls['name'].value) && controls['name'].value.length > 0) {
-            validationMessages['rfvName'].textContent = "";
+        if (rxpPhone.test(controls['phone'].value) && controls['phone'].value.length > 0) {
+            validationMessages['rfvPhone'].textContent = "";
         }
 
         //Email Validation
@@ -43,27 +43,20 @@
         else {
             validationMessages['rfvEmail'].textContent = "";
         }
-        if (!rxpName.test(controls['email'].value) && controls['email'].value.length > 0) {
+        if (!rxpPhone.test(controls['email'].value) && controls['email'].value.length > 0) {
             validationMessages['rfvEmail'].textContent = "Needs a valid email";
         }
-        if (!rxpName.test(controls['email'].value) && controls['email'].value.length > 0) {
+        if (!rxpPhone.test(controls['email'].value) && controls['email'].value.length > 0) {
             validationMessages['rfvEmail'].textContent = "";
         }
 
-        //Subject Validation
-        if (controls['subject'].value.length == 0) {
-            validationMessages['rfvSubject'].textContent = "Subject is required"
+        //Address Validation
+        if (controls['address'].value.length == 0) {
+            validationMessages['rfvAddress'].textContent = "Address is required"
         }
         else {
-            validationMessages['rfvSubject'].textContent = ""
+            validationMessages['rfvAddress'].textContent = ""
         }
 
-        //Message Validation
-        if (controls['message'].value.length == 0) {
-            validationMessages['rfvMessage'].textContent = "Message is required"
-        }
-        else {
-            validationMessages['rfvMessage'].textContent = ""
-        }
     };
 }
